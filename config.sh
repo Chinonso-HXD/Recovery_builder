42771a6a64124ac2f08f4972ca51df95d57d7f48
Chinonso-HXD <149972125+Chinonso-HXD@users.noreply.github.com>
2023-11-06 00:46:42 +0100

    config.sh

--- config.sh
+++ config.sh
@@ -1,17 +1,17 @@
 #!/bin/bash env
 
 # about the custom recovery
-export NAME="twrp" # supported: pbrp, shrp, twrp
-export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git" # the link of manifest
+export NAME="shrp" # supported: pbrp, shrp, twrp
+export MANIFEST="https://github.com/SHRP/manifest.git" # the link of manifest
 export BRANCH="twrp-9.0" # the branch of manifest
 
 # about your device
-export DEVICE="m10lte" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
-export DT_BRANCH="twrp" # device tree branch
-export VENDOR="samsung" # device manufacturer or vendor
+export DEVICE="x657c" # codename used in device tree
+export DT_LINK="https://github.com/twrpdtgen/android_device_infinix_Infinix-X657C/tree/full_x657c_h6117-user-10-QP1A.190711.020-319582-release-keys" # device tree link
+export DT_BRANCH="twrpdtgen" # device tree branch
+export VENDOR="Infinix" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
-export TYPE="omni" # recovery type: omni, twrp, something else
+export TYPE="omni_x657c" # recovery type: omni, twrp, something else
 
 # you dont need to edit these
 export SYM="_"
