6fcbb0ea8d06f5b5a88824c0268a7f0684b10303
Chinonso-HXD <149972125+Chinonso-HXD@users.noreply.github.com>
2023-11-06 13:38:50 +0100

    config.sh

--- config.sh
+++ config.sh
@@ -1,14 +1,14 @@
 #!/bin/bash env
 
 # about the custom recovery
-export NAME="shrp" # supported: pbrp, shrp, twrp
-export MANIFEST="https://github.com/SHRP/manifest.git" # the link of manifest
-export BRANCH="v3_11.0" # the branch of manifest
+export NAME="pbrp" # supported: pbrp, shrp, twrp
+export MANIFEST="https://github.com/PitchBlackRecoveryProject/manifest_pb.git" # the link of manifest
+export BRANCH="android-10.0" # the branch of manifest
 
 # about your device
-export DEVICE="x657c" # codename used in device tree
-export DT_LINK="https://github.com/twrpdtgen/android_device_infinix_Infinix-X657C/tree/full_x657c_h6117-user-10-QP1A.190711.020-319582-release-keys" # device tree link
-export DT_BRANCH="twrpdtgen" # device tree branch
+export DEVICE="Infinix-X657C" # codename used in device tree
+export DT_LINK="https://github.com/Chinonso-HXD/android_device_infinix_Infinix-X657C" # device tree link
+export DT_BRANCH="X657C" # device tree branch
 export VENDOR="Infinix" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="omni_x657c" # recovery type: omni, twrp, something else
